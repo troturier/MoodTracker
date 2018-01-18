d79120251d4bdb1f371c02d1a306e8df06023a86
Thibault Roturier <thibaultroturier@gmail.com>
2018-01-18 23:38:32 +0100

    Modification du layout de la MainActivity - Ajout d'un ViewPager pour pouvoir faire défiler les différentes humeurs à l'aide d'un swipe vertical (Création des fichiers : FragmentSmileyScreen.java et fragment_smiley_screen.xml) - Modification de MainActivity.java et activity_main.xml pour créer les éléments du ViewPager - Ajout de dépendances au build.gradle pour pouvoir créer un ViewPager vertical - Ajout des 2 boutons (inactifs) à la MainActivity (modification de activity_main.xml)

---
+++ app/src/main/java/thibault_roturier/moodtracker/FragmentSmileyScreen.java
@@ -0,0 +1,46 @@
+package thibault_roturier.moodtracker;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Thibault on 18/01/2018.
+ * Project for the Ministry of Happiness and Good Mood (OpenClassrooms).
+ */
+
+public class FragmentSmileyScreen extends Fragment {
+    private int image;
+    private int background;
+
+    public static FragmentSmileyScreen newInstance(int background, int resImage) {
+        FragmentSmileyScreen fragment = new FragmentSmileyScreen();
+        Bundle args = new Bundle();
+        args.putInt("image", resImage);
+        args.putInt("background", background);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        image = getArguments().getInt("image", 0);
+        background = getArguments().getInt("background");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_smiley_screen, container, false);
+        view.setBackgroundColor(getResources().getColor(background));
+        ImageView imageView = view.findViewById(R.id.imgMain);
+        imageView.setImageResource(image);
+        return view;
+    }
+}
--- app/src/main/java/thibault_roturier/moodtracker/MainActivity.java
+++ app/src/main/java/thibault_roturier/moodtracker/MainActivity.java
@@ -1,13 +1,81 @@
 package thibault_roturier.moodtracker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.ToxicBakery.viewpager.transforms.FlipVerticalTransformer;
+import com.ToxicBakery.viewpager.transforms.RotateUpTransformer;
+import com.ToxicBakery.viewpager.transforms.StackTransformer;
+import com.ToxicBakery.viewpager.transforms.TabletTransformer;
+import com.ToxicBakery.viewpager.transforms.ZoomInTransformer;
+
+import fr.castorflex.android.verticalviewpager.VerticalViewPager;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentPagerAdapter adapterViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        VerticalViewPager vpPager = findViewById(R.id.vpPager);
+        adapterViewPager = new MyPagerAdapter(getSupportFragmentManager());
+        vpPager.setAdapter(adapterViewPager);
+
+        vpPager.setCurrentItem(3);
+       // vpPager.setPageTransformer(true, new VerticalPageTransformer());
+    }
+
+    public static class MyPagerAdapter extends FragmentPagerAdapter {
+        private static int NUM_ITEMS = 5;
+
+        MyPagerAdapter(FragmentManager fragmentManager) {
+            super(fragmentManager);
+        }
+
+        // Returns total number of pages.
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        // Returns the fragment to display for a particular page.
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 3:
+                    return FragmentSmileyScreen.newInstance(R.color.light_sage, R.mipmap.smiley_happy);
+                case 4:
+                    return FragmentSmileyScreen.newInstance(R.color.banana_yellow, R.mipmap.smiley_super_happy);
+                case 2:
+                    return FragmentSmileyScreen.newInstance(R.color.cornflower_blue_65, R.mipmap.smiley_normal);
+                case 1:
+                    return FragmentSmileyScreen.newInstance(R.color.warm_grey, R.mipmap.smiley_disappointed);
+                case 0:
+                    return FragmentSmileyScreen.newInstance(R.color.faded_red, R.mipmap.smiley_sad);
+
+                default:
+                    return null;
+            }
+        }
+
+        // Returns the page title for the top indicator
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Tab " + position;
+        }
+
     }
 }
+
+
