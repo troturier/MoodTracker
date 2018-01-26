8a438b3559b8b153681f6be46ade1c0941b71c9f
Thibault Roturier <thibaultroturier@gmail.com>
2018-01-26 13:04:38 +0100

    Création de la classe Mood

---
+++ app/src/main/java/thibault_roturier/moodtracker/Mood.java
@@ -0,0 +1,44 @@
+package thibault_roturier.moodtracker;
+
+import java.util.Date;
+
+/**
+ * Created by Thibault on 26/01/2018.
+ * Project for the Ministry of Happiness and Good Mood (OpenClassrooms).
+ */
+
+public class Mood {
+    private Date date;
+    private int moodState;
+    private String comment;
+
+    public Mood(Date dateP, int moodStateP, String commentP){
+        this.date = dateP;
+        this.moodState = moodStateP;
+        this.comment = commentP;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public int getMoodState() {
+        return moodState;
+    }
+
+    public void setMoodState(int moodState) {
+        this.moodState = moodState;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
